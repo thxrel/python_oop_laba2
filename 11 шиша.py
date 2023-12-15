a5e86f8c31b03ef18e12d6f76f15c89642de37e4
thxrel <148333080+thxrel@users.noreply.github.com>
2023-12-15 21:18:07 +0300

    Delete 11 шиша.py

--- 11 шиша.py
+++
@@ -1,9 +0,0 @@
-class Employee:
-    def __init__(self, name, salary):
-        self.name = name
-        self.salary = salary
-    def show_info(self):
-        print(f"Имя: {self.name}")
-        print(f"Зарплата: {self.salary}")
-emp = Employee("temka", 70000)
-emp.show_info()
